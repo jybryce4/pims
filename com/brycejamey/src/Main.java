24baefdacc93fdf3960b4bb36ee195f470c1e1cc
jybryce4 <jybryce4@gmail.com>
2019-01-18 22:06:04 -0600

    added PimsSession class to run core program

--- com/brycejamey/src/Main.java
+++ com/brycejamey/src/Main.java
@@ -3,6 +3,13 @@ package com.brycejamey.src;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+         * greet user function needs to be called here
+         * 
+         * 
+         */
+
+        PimsSession p = new PimsSession(); // main process
 
     }
 }
